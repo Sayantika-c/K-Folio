be38cab9e7c2b473a1f753f10c42259f82c6d63b
Ayush Pathak <ayushnawalpathak23@gmail.com>
2026-01-04 17:23:41 +0530

    App.tsx updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import LandingPage from "./pages/Landing/LandingPage";
 import "./App.css";
 import SignIn from "./components/SignIn";
+import SignUpcard from "./components/Signup_card";
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
           Home
         </Link>
         <Link to="/signin">Sign In</Link>
+        <Link to="/signup">Sign Up</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUpcard />} />
       </Routes>
     </BrowserRouter>
   );
